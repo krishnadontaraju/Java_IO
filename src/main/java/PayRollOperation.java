e6fd20e4d1b870ab80aa5926a9d07e5a5e4deaf9
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-04-18 15:30:40 +0530

    ADDED : read and write to console

--- src/main/java/PayRollOperation.java
+++ src/main/java/PayRollOperation.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class PayRollOperation {
+
+    private List<PayRoll> payRollList;
+
+    public PayRollOperation(){}
+
+    public PayRollOperation(List<PayRoll> payRollList){}
+
     public static void main(String[] args) {
-        System.out.println("WELCOME TO THE PAY ROLL OPERATION CENTER, LET'S ROLL ");
+
+        ArrayList<PayRoll> payRollArrayList = new ArrayList<>();
+        PayRollOperation newPayRollOperation = new PayRollOperation(payRollArrayList);
+        Scanner fetch = new Scanner(System.in);
+
+        newPayRollOperation.readPayRollDetails(fetch);
+
+        newPayRollOperation.writePayRollDetails();
+    }
+
+    private void readPayRollDetails(Scanner inputFetcher){
+        System.out.println("WHAT IS THE EMPLOYEE ID ?");
+        int employeeId = inputFetcher.nextInt();
+
+        System.out.println("WHAT IS THE NAME OF THE EMPLOYEE ?");
+        String employeeName = inputFetcher.next();
+
+        System.out.println("WHAT IS THE SALARY OF THE EMPLOYEE ?");
+        double employeeSalary = inputFetcher.nextDouble();
+    }
+
+    private void writePayRollDetails(){
+        System.out.println("THE DETAILS OF THE PAYROLL ARE : \n"+payRollList);
     }
 }
